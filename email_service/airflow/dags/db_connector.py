7a8994cf01151877652bfb240073b4db3d3ae411
Milan Bhardwaj <42268449+milan-15697@users.noreply.github.com>
2021-07-03 13:18:54 +0530

    Update db_connector.py

--- email_service/airflow/dags/db_connector.py
+++ email_service/airflow/dags/db_connector.py
@@ -7,15 +7,11 @@ class DB_Connector():
     """Class responsible for gathering connections"""
     logger = logging.getLogger('master')
 
-    def connect_to_psql(self,target_db=None):
+    def connect_to_psql(self,target_db=os.environ['HOTPATH_DB_NAME']):
         """Returns a postgres connection object"""
         try:
             if target_db:
                 return psycopg2.connect("dbname="+target_db+\
-                    " user="+os.environ['HOTPATH_DB_USER']+" password="+\
-                    os.environ['HOTPATH_DB_PASS']+" host="+os.environ['HOTPATH_DB_HOST'])
-            else:
-                return psycopg2.connect("dbname="+os.environ['HOTPATH_DB_NAME']+\
                     " user="+os.environ['HOTPATH_DB_USER']+" password="+\
                     os.environ['HOTPATH_DB_PASS']+" host="+os.environ['HOTPATH_DB_HOST'])
         except (Exception,psycopg2.OperationalError) as ex:
