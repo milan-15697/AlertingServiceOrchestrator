65e07dd1ffa849c4b0a3056277af170024cefa8c
Milan Bhardwaj <42268449+milan-15697@users.noreply.github.com>
2021-07-03 13:19:40 +0530

    Update db_connector.py
    enhancing db connection method, removed extra else block and updated function argument with default os.environ

--- email_service/airflow/dags/db_connector.py
+++ email_service/airflow/dags/db_connector.py
@@ -10,9 +10,8 @@ class DB_Connector():
     def connect_to_psql(self,target_db=os.environ['HOTPATH_DB_NAME']):
         """Returns a postgres connection object"""
         try:
-            if target_db:
-                return psycopg2.connect("dbname="+target_db+\
-                    " user="+os.environ['HOTPATH_DB_USER']+" password="+\
-                    os.environ['HOTPATH_DB_PASS']+" host="+os.environ['HOTPATH_DB_HOST'])
+            return psycopg2.connect("dbname="+target_db+\
+                " user="+os.environ['HOTPATH_DB_USER']+" password="+\
+                os.environ['HOTPATH_DB_PASS']+" host="+os.environ['HOTPATH_DB_HOST'])
         except (Exception,psycopg2.OperationalError) as ex:
             self.logger.exception('Error while connection with Postgresql %s',ex)
